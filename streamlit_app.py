be261284adc815c56e4c1f0aa33d2863f66cf8b0
Hugh Gallagher <hugh.gallagher@snowflake.com>
2025-07-21 09:28:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if ingredients_list:
 # New section to display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_contsainer_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
